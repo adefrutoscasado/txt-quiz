4b90c9d70c7c8974b4c74a4a10f812328bbfd33e
unknown <alejandro.defrutos@adfrutoscasado650w10.babel-si.local>
2020-05-11 09:47:54 +0200

    Updates

---
+++ precache-manifest.761c76cdc4bff8b42f2625ea9dae53bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7268daf170aaf7e13cd95b4c08ddef22",
+    "url": "/txt-quiz/index.html"
+  },
+  {
+    "revision": "a7497047b1a76c4e5faf",
+    "url": "/txt-quiz/static/css/main.d706bab1.chunk.css"
+  },
+  {
+    "revision": "46acad08d39eb2583703",
+    "url": "/txt-quiz/static/js/2.f7b31820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/txt-quiz/static/js/2.f7b31820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7497047b1a76c4e5faf",
+    "url": "/txt-quiz/static/js/main.4de05b8c.chunk.js"
+  },
+  {
+    "revision": "bf7813a084fd3ed86bd4",
+    "url": "/txt-quiz/static/js/runtime-main.55783163.js"
+  }
+]);
